91506fd1b888ba61d9e66e5f16a1ed0ed479e41d
Maxi Candelero <maxicandelero@gmail.com>
2020-07-11 20:33:54 -0300

    Delete unnecessary usings

--- Startup.cs
+++ Startup.cs
@@ -1,17 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using webapi_m_sqlserver.Domain.Repositories;
 using webapi_m_sqlserver.Domain.Services;
@@ -49,8 +42,8 @@ namespace webapi_m_sqlserver
                 c.SwaggerDoc("v1", new OpenApiInfo
                     {
                         Version = "v1",
-                        Title = "WebAPI-M-SqlServer",
-                        Description = "DDD WebAPI Template for SQL Server",
+                        Title = "Web API Template",
+                        Description = "ASP.NET Core Web API Template (DDD)",
                     });
             });
 
@@ -80,7 +73,7 @@ namespace webapi_m_sqlserver
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI-M-SqlServer");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Web API Template");
             });
         }
     }
